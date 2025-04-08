8a13b95c1f53efe4cef533403ce2a666aacb0375
MarvinEsteban <tommyesgon0202@gmail.com>
2025-04-08 11:25:38 +0200

    error rel profile.php with profile.css fixed

--- view/profile.php
+++ view/profile.php
@@ -47,7 +47,7 @@ if (!$fetch) {
 
     <!-- Archivos CSS -->
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/profile.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 
